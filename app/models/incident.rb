2ec7c7155fe55b44d24d0099514137f73116ee4f
Hayley <hayleyanderson@zoho.com>
2014-12-04 14:17:40 -0500

    Add geocoding
    * Add geocoder gem and form query to geocoder
    * Change table/parser to store necessary and correctly formatted data
    * Change query to get relevant information from relevant entries

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,8 @@
 class Incident < ActiveRecord::Base
+  geocoded_by :cross_streets
+  after_validation :geocode, if: ->(incident){ incident.latitude == 0.0 }
+
+  def cross_streets
+    "#{on_street_name} and #{cross_street_name}, #{borough}, NY #{zip_code}"
+  end
 end
--- app/models/incident_parser.rb
+++ app/models/incident_parser.rb
@@ -5,14 +5,18 @@ class IncidentParser
 
   def parse
     {
+      borough: @raw_incident["borough"],
       cyclists_injured: @raw_incident["number_of_cyclist_injured"].to_i,
       cyclists_killed: @raw_incident["number_of_cyclist_killed"].to_i,
       date: @raw_incident["date"],
       key: @raw_incident["unique_key"],
-      latitude: @raw_incident["latitude"],
-      longitude: @raw_incident["longitude"],
+      latitude: @raw_incident["latitude"].to_f,
+      longitude: @raw_incident["longitude"].to_f,
+      cross_street_name: @raw_incident["off_street_name"],
+      on_street_name: @raw_incident["on_street_name"],
       pedestrians_injured: @raw_incident["number_of_pedestrians_injured"].to_i,
       pedestrians_killed: @raw_incident["number_of_pedestrians_killed"].to_i,
+      zip_code: @raw_incident["zip_code"]
     }
   end
 end
